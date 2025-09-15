dddfdf6523da90ef3ea75e84e70043333d82f142
John Wheeler <johnwheeler@Johns-MBP.lan>
2025-09-15 13:38:52 -0400

    chore(fix): build errors from pubdate mis-formatting

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -4,8 +4,8 @@ import { getCollection } from 'astro:content';
 export async function GET(context) {
   const blog = await getCollection('blog');
   return rss({
-    title: 'Brutal Blog',
-    description: 'Brutal is a theme for Astro',
+    title: 'The Blog',
+    description: 'John Wheeler writes about the web, SEO, AEO, rock climbing and Duneons & Dragons.',
     stylesheet: false,
     site: context.site,
     items: blog.map((post) => ({
@@ -15,6 +15,6 @@ export async function GET(context) {
       link: `/blog/${post.slug}/`,
     })),
     customData: '<language>en-us</language>',
-    canonicalUrl: 'https://brutal.elian.codes',
+    canonicalUrl: 'https://johnwheelerproduction.com',
   });
 }
